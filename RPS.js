56ca9503f952e9c667af1428c83d769655541f89
Mizu <124433727+MIzuw@users.noreply.github.com>
2023-08-30 16:25:20 +0200

    playRound Function

--- RPS.js
+++ RPS.js
@@ -1,14 +1,31 @@
 function getComputerChoice() {
     var ComputerChoice = Math.floor(Math.random() * 4)
     if (ComputerChoice == 1) {
-        ComputerChoice = "Rock"
+        ComputerChoice = "rock"
     }
     else if (ComputerChoice == 2 ) {
-        ComputerChoice = "Stone"
+        ComputerChoice = "paper"
     }
     else {
-        ComputerChoice = "Scissors"
+        ComputerChoice = "scissors"
     }
 
     return ComputerChoice
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection); {
+        return "It's a Tie!"
+    }
+    
+    switch(true) {
+        case playerSelection == 'rock' && computerSelection == 'scissors':
+        case playerSelection == 'paper' && computerSelection == 'rock':
+        case playerSelection == 'scissors' && computerSelection == 'paper':
+            return `${playerSelection} beats ${computerSelection}! You won!`;
+        break;
+
+        default:
+            return `${computerSelection} beats ${playerSelection}! You lose!`
+    }
 }
