212ffc54109dd42e678d2aec103cb6516d1ec226
Mizu <124433727+MIzuw@users.noreply.github.com>
2023-08-30 17:52:24 +0200

    score system added

--- RPS.js
+++ RPS.js
@@ -1,3 +1,5 @@
+var PlayerScore = 0
+var ComputerScore = 0
 function getComputerChoice() {
     var ComputerChoice = Math.floor(Math.random() * 4)
     if (ComputerChoice == 1) {
@@ -22,23 +24,29 @@ function playRound(playerSelection, computerSelection) {
         case playerSelection == 'rock' && computerSelection === 'scissors':
         case playerSelection == 'paper' && computerSelection == 'rock':
         case playerSelection == 'scissors' && computerSelection == 'paper':
-            return `${playerSelection} beats ${computerSelection}! You won!`;
-
+            PlayerScore++
+            return `${playerSelection} beats ${computerSelection}! You won!`
         default:
+            ComputerScore++
             return `${computerSelection} beats ${playerSelection}! You lose!`
     }
 }
 
 function game() {
-
     for (let i = 1; i < 6; i++) {
         console.log("Round " + i + " from 5") 
-
+        
         var playerSelection = prompt("Rock, Paper or Scissors?")
         var computerSelection = getComputerChoice();
 
         play = playRound(playerSelection.toLowerCase(), computerSelection)
         console.log(play)
     }
+    if (PlayerScore > ComputerScore) {
+        console.log('You won!')
+    } else {
+        console.log('You lost!')
+    } 
+    console.log(`You have a Score of ${PlayerScore}, while the Computer has a Score of ${ComputerScore}`)
 }
 
