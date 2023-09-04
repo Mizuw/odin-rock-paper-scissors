c65eb5fbb1fe951d251886d8472a790b8ad4cc74
Mizu <124433727+MIzuw@users.noreply.github.com>
2023-09-04 11:23:20 +0200

    simple UI working
    still uses the console, but the buttons are working

--- RPS.js
+++ RPS.js
@@ -1,5 +1,35 @@
-let PlayerScore = 0 
-let ComputerScore = 0
+let PlayerScore = 0; 
+let ComputerScore = 0;
+
+const container = document.querySelector('#container');
+const buttons = document.querySelector('#buttons');
+
+function game() {   
+    let playerSelection = this.document.activeElement.getAttribute("id");
+    let computerSelection = getComputerChoice();
+    switch(true) {  // checks the user input, only allows "rock", "paper" or "scissors" (case insensitive because of the .toLowerCase())
+        case playerSelection.toLowerCase() == 'rock':
+        case playerSelection.toLowerCase() == 'paper':
+        case playerSelection.toLowerCase() == 'scissors':
+            play = playRound(playerSelection, computerSelection)
+            console.log(`You choose ${playerSelection}, while the Computer choose ${computerSelection}!`)
+            console.log(play)
+
+            console.log(`You have a Score of ${PlayerScore}, while the Computer has a Score of ${ComputerScore}`)
+            if (PlayerScore > ComputerScore) {
+                console.log('You won!')
+            } else if (PlayerScore == ComputerScore) {
+                console.log('It\'s a Tie Game!')
+            } else {
+                console.log('You lost!')
+            } 
+        break;
+        default:
+            playerSelection-- // for some reason a point to the playerselection will be added when they input something that isn't allowed by the switch statement, so we remove this point. I'm not entirely sure why the point is added, but this works as a fix
+            console.log("Please enter Rock, Paper or Scissors")
+    }
+}
+
 function getComputerChoice() {
     let ComputerChoice = Math.floor(Math.random() * 4) // this variable chooses a random number from 1-3, after that we can assign the numbers rock paper or scissors, which then would be the choice of the computer (as seen in the if statements in the next lines)
     if (ComputerChoice == 1) {
@@ -28,35 +58,4 @@ function playRound(playerSelection, computerSelection) {
             ComputerScore++
             return `${computerSelection} beats ${playerSelection}! You lose!`
     }
-}
-
-function game() {
-    for (let i = 1; i < 6; i++) { // will loop until 'i' has a value of 6, which would be on round 5
-        console.log("Round " + i + " from 5") 
-        
-        let playerSelection = prompt("Rock, Paper or Scissors?")
-        let computerSelection = getComputerChoice();
-        switch(true) {  // checks the user input, only allows "rock", "paper" or "scissors" (case insensitive because of the .toLowerCase())
-            case playerSelection.toLowerCase() == 'rock':
-            case playerSelection.toLowerCase() == 'paper':
-            case playerSelection.toLowerCase() == 'scissors':
-                play = playRound(playerSelection, computerSelection)
-                console.log(`You choose ${playerSelection}, while the Computer choose ${computerSelection}!`)
-                console.log(play)
-            break;
-            default:
-                i-- // the for loop would continue to the next round regardless, so we subtract one round. Because of that the current round will loop until the user inputs correctly
-                playerSelection-- // for some reason a point to the playerselection will be added when they input something that isn't allowed by the switch statement, so we remove this point. I'm not entirely sure why the point is added, but this works as a fix
-                console.log("Please enter Rock, Paper or Scissors")
-        }
-    }
-
-    console.log(`You have a Score of ${PlayerScore}, while the Computer has a Score of ${ComputerScore}`)
-    if (PlayerScore > ComputerScore) {
-        console.log('You won!')
-    } else if (PlayerScore == ComputerScore) {
-        console.log('It\'s a Tie Game!')
-    } else {
-        console.log('You lost!')
-    } 
 }
